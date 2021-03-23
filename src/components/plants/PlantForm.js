d47e2eada95b51f4ad1e210038f00cbdc3099ab3
Gabriel <bultaqi@gmail.com>
2021-03-23 11:00:39 -0400

    update and add component fixed when clicked on for details.

--- src/components/plants/PlantForm.js
+++ src/components/plants/PlantForm.js
@@ -7,58 +7,64 @@ import { useHistory, useParams } from 'react-router-dom';
 export const PlantForm = () => {
     const { addPlant, getPlantById, updatePlant } = useContext(PlantContext)
 
-    //for edit, hold on to state of animal in this view
+    //for edit, hold on to state of plants in this view
     const [plant, setPlants] = useState({})
     
     //wait for data before button is active
     const [isLoading, setIsLoading] = useState(true);
 
+    //return an object of the params for the route rendered
     const {plantId} = useParams();
+
+    // the history hook for managing session history
 	  const history = useHistory();
 
-    //when field changes, update state. This causes a re-render and updates the view.
+    //when an input field changes, update state. This causes a re-render and updates the view.
     //Controlled component
     const handleControlledInputChange = (event) => {
-      //When changing a state object or array,
-      //always create a copy make changes, and then set state.
+
+      //create a copy of a piece of state and THEN set state with the function that changes it
+      //in this case...an object, in an array of objects of plants
       const newPlant = { ...plant }
-      //animal is an object with properties.
-      //set the property to the new value
+
+      //plant is an object with properties. set the property to the new value
       newPlant[event.target.name] = event.target.value
-      //update state
+
+      //call the function that updates state
       setPlants(newPlant)
     }
 
     const handleSavePlant = () => {
-      if (parseInt(plant.locationId) === 0) {
-          window.alert("Please select a location")
+      
+      //in the case, a plant in the array we are updating || adding it to the strigified database
+      if (plant.commonName === "" || plant.scientificName === "") {
+          window.alert("Please enter a name")
       } else {
-        //disable the button - no extra clicks
+        //disable the button - no extra clicks to fuck shit up
         setIsLoading(true);
         if (plantId){
-          //PUT - update
+          //"PUT" method from the context provider -UPDATE
           updatePlant({
               id: plant.id,
-              name: plant.name,
-              breed: plant.breed,
-              locationId: parseInt(plant.locationId),
-              customerId: parseInt(plant.customerId)
+              commonName: plant.commonName,
+              scientificName: plant.scientificName
           })
-          .then(() => history.push(`/animals/detail/${plant.id}`))
-        }else {
-          //POST - add
+          //pushes a new entry onto the history stack
+          .then(() => history.push(`/plants/detail/${plant.id}`))
+        } else {
+          //"POST" method from the context provider -ADD
           addPlant({
               name: plant.name,
-              breed: plant.breed,
-              locationId: parseInt(plant.locationId),
-              customerId: parseInt(plant.customerId)
+              commonName: plant.commonName,
+              scientificName: plant.scientificName
           })
+          //pushes a new entry onto the history stack
           .then(() => history.push("/plants"))
         }
       }
     }
 
-    // Get customers and locations. If animalId is in the URL, getAnimalById
+    // Get plants. If plantId is in the URL, getPlantById
     useEffect(() => {
         if (plantId){
           getPlantById(plantId)
@@ -72,19 +78,27 @@ export const PlantForm = () => {
     
     }, [])
 
-    //since state controlls this component, we no longer need
-    //useRef(null) or ref
 
+    //since state controlls this component, we no longer need useRef(null) or ref
     return (
-      <form className="animalForm">
-        <h2 className="animalForm__title">New Plant</h2>
+      <form className="plantForm">
+        <h2 className="plantForm__title">New Plant</h2>
         <fieldset>
           <div className="form-group">
-            <label htmlFor="animalName">Plant name: </label>
-            <input type="text" id="animalName" name="name" required autoFocus className="form-control"
-            placeholder="Plant name"
+            <label htmlFor="plantCommonName">Plant common name: </label>
+            <input type="text" id="commonName" name="commonName" required autoFocus className="form-control"
+            placeholder="Common name for plant"
             onChange={handleControlledInputChange}
-            defaultValue={plant.name}/>
+            defaultValue={plant.commonName}/>
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <label htmlFor="plantScientificName">Plant scientific name: </label>
+            <input type="text" id="scientificName" name="scientificName" required autoFocus className="form-control"
+            placeholder="Scientific name for plant"
+            onChange={handleControlledInputChange}
+            defaultValue={plant.scientificName}/>
           </div>
         </fieldset>
         <button className="btn btn-primary"
@@ -93,7 +107,7 @@ export const PlantForm = () => {
             event.preventDefault() // Prevent browser from submitting the form and refreshing the page
             handleSavePlant()
           }}>
-        {plantId ? <>Save Plant</> : <>Add Plant</>}</button>
+        {plantId ? <>Save</> : <>Add Plant</>}</button>
       </form>
     )
 }
