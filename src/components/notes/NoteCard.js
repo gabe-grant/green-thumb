7d386383e4067051f192303e36e79ae579fe3c87
Gabriel <bultaqi@gmail.com>
2021-03-26 13:42:17 -0400

    added delete button for the note cards

--- src/components/notes/NoteCard.js
+++ src/components/notes/NoteCard.js
@@ -1,17 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useHistory, useParams } from "react-router"
 import "./Note.css"
+import { NoteContext } from './NoteProvider'
 
 
 //a prop called note is being passed into NoteCard
 //that singleNote filtered and mapped from NoteList will display here
 export const NoteCard = ({ note }) => {
+    const {deleteNote} = useContext(NoteContext)
+
+    const handleDelete = () => {
+        deleteNote(note.id)
+    }
    
     return (
+    <>
     <section className="note">
         <h3 className="note__name"></h3>
         <p>{note.date}</p>
         <p>{note.message}</p>
     </section>
+    <button onClick={handleDelete}>Delete note</button>
+    </>   
     )
 }
