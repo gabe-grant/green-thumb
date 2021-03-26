a836de84f83d0d965b81e6d821bce86d3a7ac679
Gabriel <bultaqi@gmail.com>
2021-03-26 13:48:18 -0400

    changed the details link to a button on the plant card.

--- src/components/plants/PlantCard.js
+++ src/components/plants/PlantCard.js
@@ -1,7 +1,12 @@
 import React from "react"
 import "./Plant.css"
-import { Link } from "react-router-dom"
 import { useHistory } from "react-router-dom"
+
+
+// import { Link } from "react-router-dom"
+// <h3 className="plant__name">
+//           <Link to={`/plants/detail/${plantProp.id}`}>{plantProp.commonName}</Link>
+// </h3>
 
 export const PlantCard = ({ plantProp }) => {
 
@@ -10,13 +15,13 @@ export const PlantCard = ({ plantProp }) => {
   
   return (
     <section className="plant">
-        <h3 className="plant__name">
-          <Link to={`/plants/detail/${plantProp.id}`}>{plantProp.commonName}</Link>
-        </h3>
+        <h3 className="plant__name">{plantProp.commonName}</h3>
         <h5>{plantProp.scientificName}</h5>
         <p>{plantProp.description}</p>
         <p>{plantProp.careInstructions}</p>
-        <button className="notesDetails" onClick={() => {history.push(`/notes/${plantProp.id}`)}}>Notes -->
+        <button className="plantButtons" onClick={() => {history.push(`/notes/${plantProp.id}`)}}>Notes -->
+        </button>
+        <button className="plantButtons" onClick={() => {history.push(`/plants/detail/${plantProp.id}`)}}>Details -->
         </button>
     </section>
   )
